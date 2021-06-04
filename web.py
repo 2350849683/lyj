96e1b1074273c5ee6959b216d3502327a492c03e
silentmx <2350849683@qq.com>
2021-06-04 15:07:03 +0800

    2

--- web.py
+++ web.py
@@ -10,9 +10,13 @@ web = Blueprint('psdash', __name__)
 def register():
     nodes=request.remote_addr + ":" + request.args["port"]
     if nodes not in current_app.psdash.nodes:
-        current_app.psdash.register_agent(nodes)
+        current_app.psdash.register_agent(nodes,request.args["name"])
     return ""
 
 @web.route("/nodes")
 def nodes():
-    return str(current_app.psdash.nodes)
+    a=[]
+    for node in  current_app.psdash.nodes:
+        node=node.split(":")
+        a.append({"hostname":node[0],"host":node[1],"port":int(node[2])})
+    return str(a)
